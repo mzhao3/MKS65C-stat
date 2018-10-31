ba3358e2831481b11538a13cd5f113026bebb4e0
wzhou2 <wzhou2@stuy.edu>
2018-10-30 20:14:16 -0400

    fixed error with malloc

--- stat.c
+++ stat.c
@@ -3,23 +3,22 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <time.h>
+#include <errno.h>
+
 int main() {
-  //printf("FD: %d\n", a);
+    struct stat * statpointer = malloc(sizeof(struct stat *));
 
-  struct stat * statpointer;
+    int b = stat("hello.txt", statpointer);
 
-  int b = stat("/README.md", statpointer);
+    printf("Value of pointer: %d\n", b);
+    printf("Size: %ld\n", statpointer->st_size);
+    printf("Size: %ldMB\n", (statpointer->st_size)/1000);
+    printf("UserID: %d\n", statpointer->st_uid);
+    printf("GroupID: %d\n", statpointer->st_gid);
+    printf("Mode: %o\n", statpointer->st_mode);
+    printf("Time last accessed:%s", ctime(&(statpointer->st_atime)) );
+    printf("Time last accessed:%ld\n", statpointer->st_atime );
 
-  //char * stringbean[256];
-
-  printf(": %d\n", b);
-  printf("Size: %d\n", statpointer->st_size);
-  printf("Size: %dMB\n", (statpointer->st_size)/1000);
-  printf("UserID: %d\n", statpointer->st_uid);
-  printf("GroupID: %d\n", statpointer->st_gid);
-  printf("Mode: %o\n", statpointer->st_mode);
-  printf("Time last accessed:%s", ctime(&(statpointer->st_atime)) );
-  printf("Time last accessed:%d\n", statpointer->st_atime );
-
-  return 0;
+    free(statpointer);
+    return 0;
 }
