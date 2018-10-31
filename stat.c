a0a9606023ad2bf1dc68a0183e32fb8877dd2c53
wzhou2 <wzhou2@stuy.edu>
2018-10-30 20:32:41 -0400

    works

--- stat.c
+++ stat.c
@@ -6,18 +6,29 @@
 #include <errno.h>
 
 int main() {
+    char * path = "hello.txt";
     struct stat * statpointer = malloc(sizeof(struct stat *));
 
-    int b = stat("hello.txt", statpointer);
+    int b = stat(path, statpointer);
 
     printf("Value of pointer: %d\n", b);
     printf("Size: %ld\n", statpointer->st_size);
-    printf("Size: %ldMB\n", (statpointer->st_size)/1000);
+    
+    /* WEIRD NUMBERS
+    printf("==============================\n");
+    printf("Size:\n");
+    printf("In B: %ld\n", statpointer->st_size);
+    printf("In KB: %ld\n", statpointer->st_size/1000);
+    printf("In MB: %ld\n", statpointer->st_size/1000000);
+    printf("In GB: %ld\n", statpointer->st_size/1000000000);
+    printf("==============================\n");
+    */
     printf("UserID: %d\n", statpointer->st_uid);
     printf("GroupID: %d\n", statpointer->st_gid);
     printf("Mode: %o\n", statpointer->st_mode);
     printf("Time last accessed:%s", ctime(&(statpointer->st_atime)) );
-    printf("Time last accessed:%ld\n", statpointer->st_atime );
+    //printf("Time last accessed:%ld\n", statpointer->st_atime );
+    
 
     free(statpointer);
     return 0;
